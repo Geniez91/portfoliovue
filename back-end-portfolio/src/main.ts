c39d51d58b9f0cf07b154c15f5b8fa440cc5b83d
Jeremy Weltmann <weltmannjeremy@gmail.com>
2025-03-14 18:26:42 +0100

    test add vercel

--- back-end-portfolio/src/main.ts
+++ back-end-portfolio/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';  // Import de NestExpressApplication
+import { NestExpressApplication } from '@nestjs/platform-express';  
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 export async function createNestApp(): Promise<NestExpressApplication> {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const options = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('Your API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addServer('https://staging.yourapi.com/', 'Staging')
+    .addServer('https://production.yourapi.com/', 'Production')
+    .addTag('Your API Tag')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
   await app.init();
   return app as NestExpressApplication; // Casting vers NestExpressApplication
 }
