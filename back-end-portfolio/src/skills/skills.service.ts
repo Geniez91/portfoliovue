cb78e6781c30cdee0b2978d6c50f266aa00f3939
Jeremy Weltmann <weltmannjeremy@gmail.com>
2025-03-14 18:09:52 +0100

    [fix] test

--- back-end-portfolio/src/skills/skills.service.ts
+++ back-end-portfolio/src/skills/skills.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from "@/prisma/prisma.service"
+import { PrismaService } from "../prisma/prisma.service"
 import {  AddSkills, Skills } from './skills.interface';
 import { plainToInstance } from 'class-transformer';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
