5c484b95dfad13b9f972a821d880c60a96c42293
Jeremy Weltmann <weltmannjeremy@gmail.com>
2025-03-17 17:32:12 +0100

    change port

--- back-end-portfolio/src/main.ts
+++ back-end-portfolio/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8080;
   await app.listen(port);
   console.log(`Application en cours d'exécution sur le port ${port}`);
 }
