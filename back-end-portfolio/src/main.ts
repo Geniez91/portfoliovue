7076ac409813335aaef8f09182fb83c147f7ee6e
Jeremy Weltmann <weltmannjeremy@gmail.com>
2025-03-17 17:39:43 +0100

    add swagger on project

--- back-end-portfolio/src/main.ts
+++ back-end-portfolio/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
+  const config=new DocumentBuilder()
+  .setTitle('Portfolio Api')
+  .setDescription('API CRUD de mon portfolio')
+  .setVersion('1.0')
+  .addTag('Portfolio')
+  .build()
+
+  const documentFactory=()=>SwaggerModule.createDocument(app,config)
+  SwaggerModule.setup('api',app,documentFactory)
   const port = process.env.PORT || 8080;
   await app.listen(port);
   console.log(`Application en cours d'exécution sur le port ${port}`);
