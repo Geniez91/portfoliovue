ea7a79c014ca00224e743d98295a6821631dd6da
Jeremy Weltmann <weltmannjeremy@gmail.com>
2025-05-28 18:22:05 +0200

    comment addProject

--- back-end-portfolio/src/project/project.controller.ts
+++ back-end-portfolio/src/project/project.controller.ts
@@ -17,20 +17,20 @@ export class ProjectController {
     return this.projetService.getAllProject();
     }
 
-    @Post()
-    @UseInterceptors(FileInterceptor('file'))
-    @ApiConsumes('multipart/form-data')
-    @UseGuards(AuthGuard)
-    async addProject(@UploadedFiles() files: Express.Multer.File[],@Body() body: any): Promise<Project> {
-              const workExperienceImgs = await this.projetService.uploadImages(files);
-                const transformed = plainToInstance(Project, {
-                ...body,
-                year: new Date(body.year),
-                stack: JSON.parse(body.stack),
-              });
-              await validateOrReject(transformed);
-              return await this.projetService.addProject(workExperienceImgs,transformed);
-    }
+    // @Post()
+    // @UseInterceptors(FileInterceptor('file'))
+    // @ApiConsumes('multipart/form-data')
+    // @UseGuards(AuthGuard)
+    // async addProject(@UploadedFiles() files: Express.Multer.File[],@Body() body: any): Promise<Project> {
+    //           const workExperienceImgs = await this.projetService.uploadImages(files);
+    //             const transformed = plainToInstance(Project, {
+    //             ...body,
+    //             year: new Date(body.year),
+    //             stack: JSON.parse(body.stack),
+    //           });
+    //           await validateOrReject(transformed);
+    //           return await this.projetService.addProject(workExperienceImgs,transformed);
+    // }
     
     
 
