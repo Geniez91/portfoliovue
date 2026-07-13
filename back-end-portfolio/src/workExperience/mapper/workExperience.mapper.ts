3a02a8ea975bff1e9e73c52dd91c7a3ce5b2ccb2
Jeremy Weltmann <weltmannjeremy@gmail.com>
2026-07-13 19:09:26 +0200

    fix workexperience mapper

--- back-end-portfolio/src/workExperience/mapper/workExperience.mapper.ts
+++ back-end-portfolio/src/workExperience/mapper/workExperience.mapper.ts
@@ -1,5 +1,9 @@
 import { plainToInstance } from "class-transformer";
 import { WorkExperience } from "../entity/workExperience.entity";
+import { Prisma } from "@prisma/client";
+import { CreateWorkExperienceDto } from "../dto/create-workExperience.dto";
+import { UpdateWorkExperienceDto } from "../dto/update-workExperience.dto";
+import { StackImgDto } from "@/project/entity/project.entity.dto";
 
 export class WorkExperienceMapper {
     static toEntity(data: any): WorkExperience {
@@ -14,17 +18,38 @@ export class WorkExperienceMapper {
         });
     }
 
-    static toCreateInput(dto:any,workExperienceImg:string):any{
+    static toCreateInput(dto:CreateWorkExperienceDto,workExperienceImg:string):Prisma.workExperienceCreateInput{
         return {
-            ...dto,
-            srcImg:workExperienceImg
+           nameCompany: dto.nameCompany,
+            job: dto.job,
+            tasks: dto.tasks,
+            startDate: dto.startDate,
+            endDate: dto.endDate,
+            stack: this.toStackJson(dto.stack),
+            content: dto.content,
+            srcImg: workExperienceImg,
         }
     }
 
-    static toUpdateInput(dto:any,workExperienceImg?:string):any{
+    static toUpdateInput(dto:UpdateWorkExperienceDto,workExperienceImg?:string):Prisma.workExperienceUpdateInput{
          return {
-            ...dto,
+            nameCompany: dto.nameCompany,
+            job: dto.job,
+            tasks: dto.tasks,
+            startDate: dto.startDate,
+            endDate: dto.endDate,
+            stack: (dto.stack && this.toStackJson(dto.stack)),
+            content: dto.content,
             ...(workExperienceImg && {srcImg: workExperienceImg})
          }
     }
+
+    private static toStackJson(
+  stack: StackImgDto[],
+): Prisma.InputJsonValue {
+  return stack.map(item => ({
+    img: item.img,
+    name: item.name,
+  })) as Prisma.InputJsonValue;
+}
 }
